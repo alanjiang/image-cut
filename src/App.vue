603f1ac9a9f92e89f5d88a8d21cb46612ef31c8e
jiangeng <jiangpeng@agilean.cn>
2020-09-23 22:49:16 +0800

    图片上传

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+   
+    <div>
+      <ImageCut></ImageCut>
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageCut from './components/ImageCut'
+export default {
+  
+  methods: {
+    
+  },
+  components:
+  {
+    ImageCut
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
